4f9e27836f258b52cec67c9af46cfbd1699d139a
Harry Goatman <hg@harrygoatman.com>
2022-12-02 11:40:08 +0000

    feat: part 2

--- 2/src/main.rs
+++ 2/src/main.rs
@@ -71,7 +71,7 @@ fn parse_round(round_str: &str) -> Round {
     Round(hand_1, hand_2)
 }
 
-fn parse_encrypted_strategy_guide(guide: String) -> Vec<Round> {
+fn parse_encrypted_strategy_guide(guide: &String) -> Vec<Round> {
     let round_strings = guide.split('\n');
 
     round_strings
@@ -129,22 +129,94 @@ fn play_tournament(guide: Vec<Round>) -> Score {
     guide.iter().fold(Score(0, 0), calculate_outcome)
 }
 
+// Part 2
+
+struct TargetRound(HandShape, Outcome);
+
+fn parse_outcome(val: char) -> Outcome {
+    match val {
+        'X' => Outcome::Lost,
+        'Y' => Outcome::Draw,
+        'Z' => Outcome::Won,
+        _ => panic!("Not a letter."),
+    }
+}
+
+fn parse_target_round(target_round_str: &str) -> TargetRound {
+    let (player_1, target_outcome) = (
+        target_round_str.chars().next().unwrap(),
+        target_round_str.chars().rev().next().unwrap(),
+    );
+
+    let (hand_1, outcome) = (parse_handshape(player_1), parse_outcome(target_outcome));
+
+    TargetRound(hand_1, outcome)
+}
+
+fn parse_ultra_top_secret_strategy_guide(guide: &String) -> Vec<TargetRound> {
+    let round_strings = guide.split('\n');
+
+    round_strings
+        .filter(|val| val != &"")
+        .map(parse_target_round)
+        .collect()
+}
+
+fn pick_loser(hand: &HandShape) -> HandShape {
+    match hand {
+        HandShape::Rock => HandShape::Paper,
+        HandShape::Paper => HandShape::Scissors,
+        HandShape::Scissors => HandShape::Rock,
+    }
+}
+
+fn pick_winner(hand: &HandShape) -> HandShape {
+    match hand {
+        HandShape::Rock => HandShape::Scissors,
+        HandShape::Paper => HandShape::Rock,
+        HandShape::Scissors => HandShape::Paper,
+    }
+}
+
+fn target_round_to_round(target_round: &TargetRound) -> Round {
+    let my_pick = match target_round.1 {
+        Outcome::Lost => pick_winner(&target_round.0),
+        Outcome::Draw => target_round.0,
+        Outcome::Won => pick_loser(&target_round.0),
+    };
+
+    Round(target_round.0, my_pick)
+}
+
+fn convert_target_rounds_to_rounds(target_rounds: Vec<TargetRound>) -> Vec<Round> {
+    target_rounds.iter().map(target_round_to_round).collect()
+}
+
 fn main() {
     let input_str = load_input();
-    let guide = parse_encrypted_strategy_guide(input_str);
+    let guide = parse_encrypted_strategy_guide(&input_str);
     let final_score = play_tournament(guide);
 
     println!("{:?}", final_score);
+
+    let guide_2 = parse_ultra_top_secret_strategy_guide(&input_str);
+    let converted_guide = convert_target_rounds_to_rounds(guide_2);
+    let part_2_scores = play_tournament(converted_guide);
+
+    println!("{:?}", part_2_scores);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_encrypted_strategy_guide, play_tournament, HandShape, Round};
+    use crate::{
+        convert_target_rounds_to_rounds, parse_encrypted_strategy_guide, play_tournament,
+        HandShape, Outcome, Round, TargetRound,
+    };
 
     #[test]
     fn test_parse_encrypted_strategy_guide() {
         let puzzle_string = String::from("A Y\nB X\nC Z\n\n");
-        let parse_result = parse_encrypted_strategy_guide(puzzle_string);
+        let parse_result = parse_encrypted_strategy_guide(&puzzle_string);
         let expected_result = vec![
             Round(HandShape::Rock, HandShape::Paper),
             Round(HandShape::Paper, HandShape::Rock),
@@ -163,5 +235,22 @@ mod tests {
 
         let scores = play_tournament(guide);
         assert_eq!(15, scores.1);
+    }
+
+    #[test]
+    fn test_convert_to_rounds() {
+        let guide = vec![
+            TargetRound(HandShape::Rock, Outcome::Draw),
+            TargetRound(HandShape::Paper, Outcome::Lost),
+            TargetRound(HandShape::Scissors, Outcome::Won),
+        ];
+
+        let target_rounds = vec![
+            Round(HandShape::Rock, HandShape::Rock),
+            Round(HandShape::Paper, HandShape::Rock),
+            Round(HandShape::Scissors, HandShape::Rock),
+        ];
+
+        assert_eq!(convert_target_rounds_to_rounds(guide), target_rounds)
     }
 }
