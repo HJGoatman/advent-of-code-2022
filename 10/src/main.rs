c2107662887e89773885c7e48cba852824642abe
Harry Goatman <hg@harrygoatman.com>
2022-12-12 15:15:41 +0000

    feat: part 2

--- 10/src/main.rs
+++ 10/src/main.rs
@@ -36,6 +36,7 @@ fn increment_clock(
     clock: &mut dyn Iterator<Item = i32>,
     register: &i32,
     signal_strengths: &mut Vec<SignalStrength>,
+    crt: &mut Vec<bool>,
 ) {
     let cycle_num = clock.next().unwrap();
     log::debug!("Cycle: {}", cycle_num);
@@ -45,27 +46,48 @@ fn increment_clock(
 
         log::debug!("During the {}th cycle, register X has the value {}, so the signal strength is {} * {} = {}.", cycle_num, register, cycle_num, register, signal_strength);
     }
+
+    let pixel = (cycle_num % 40) - 1;
+    let sprite_start = register - 1;
+    let sprite_end = register + 1;
+    let is_pixel_lit = sprite_start <= pixel && pixel <= sprite_end;
+    crt.push(is_pixel_lit);
 }
 
-fn run_program(program: &[Instruction]) -> Vec<SignalStrength> {
+fn run_program(program: &[Instruction]) -> (Vec<SignalStrength>, Vec<bool>) {
     let mut signal_strengths: Vec<SignalStrength> = vec![];
     let mut clock = 1..;
     let mut register = 1;
+    let mut crt: Vec<bool> = vec![];
 
     for instruction in program {
         if let Instruction::AddX(val) = instruction {
-            increment_clock(&mut clock, &register, &mut signal_strengths);
-            increment_clock(&mut clock, &register, &mut signal_strengths);
+            increment_clock(&mut clock, &register, &mut signal_strengths, &mut crt);
+            increment_clock(&mut clock, &register, &mut signal_strengths, &mut crt);
             register = register + val;
         } else {
-            increment_clock(&mut clock, &register, &mut signal_strengths);
+            increment_clock(&mut clock, &register, &mut signal_strengths, &mut crt);
         }
 
         log::debug!("Instruction: {:?}, X: {}", instruction, register);
         log::debug!("");
     }
 
-    signal_strengths
+    (signal_strengths, crt)
+}
+
+fn display_crt(crt: &[bool]) -> String {
+    let chars: Vec<char> = crt
+        .iter()
+        .map(|a| match a {
+            true => '#',
+            _ => '.',
+        })
+        .collect();
+
+    chars.chunks(40).fold(String::from(""), |x, y| {
+        format!("{}\n{}", x, y.to_vec().iter().collect::<String>())
+    })
 }
 
 fn main() {
@@ -73,10 +95,13 @@ fn main() {
 
     let input = load_input();
     let program = parse_program(&input);
-    let signal_strengths = run_program(&program);
+    let (signal_strengths, crt) = run_program(&program);
     let total_signal_strength: i32 = signal_strengths.iter().sum();
 
     println!("{:?}", total_signal_strength);
+
+    let display = display_crt(&crt);
+    print!("{}", display);
 }
 
 #[cfg(test)]
