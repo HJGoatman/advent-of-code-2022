af8f40a62c4e64d79572381315cd6d0ad420d148
Harry Goatman <hg@harrygoatman.com>
2022-12-13 12:18:30 +0000

    feat: part 2

--- 11/src/main.rs
+++ 11/src/main.rs
@@ -1,9 +1,10 @@
+use env_logger;
 use std::{
     collections::{HashMap, VecDeque},
     env, fs,
 };
 
-type WorryLevel = u32;
+type WorryLevel = u64;
 type MonkeyID = u8;
 
 #[derive(Debug, PartialEq)]
@@ -21,12 +22,12 @@ enum Operation {
 #[derive(Debug, PartialEq)]
 struct Monkey {
     id: MonkeyID,
-    items: Vec<WorryLevel>,
+    items: VecDeque<WorryLevel>,
     operation: Operation,
     test_value: WorryLevel,
     true_monkey_id: MonkeyID,
     false_monkey_id: MonkeyID,
-    inspections: u32,
+    inspections: u64,
 }
 
 fn load_input() -> String {
@@ -39,7 +40,7 @@ fn parse_monkey_id(input: &str) -> MonkeyID {
     id_str.parse::<MonkeyID>().unwrap()
 }
 
-fn parse_starting_items(input: &str) -> Vec<WorryLevel> {
+fn parse_starting_items(input: &str) -> VecDeque<WorryLevel> {
     let items_str = &input.trim()[16..];
     items_str
         .split(", ")
@@ -139,15 +140,26 @@ fn run_operation(operation: &Operation, item: WorryLevel) -> WorryLevel {
     }
 }
 
-fn take_turn(id: MonkeyID, monkeys: &mut HashMap<MonkeyID, Monkey>) {
+fn take_turn(
+    id: MonkeyID,
+    monkeys: &mut HashMap<MonkeyID, Monkey>,
+    is_part_1: bool,
+    mod_value: u64,
+) {
     let monkey = monkeys.get_mut(&id).unwrap();
 
     let mut deq = VecDeque::new();
 
-    while let Some(item) = monkey.items.pop() {
+    while let Some(item) = monkey.items.pop_front() {
         let mut worry_level = run_operation(&monkey.operation, item);
+
         monkey.inspections = monkey.inspections + 1;
-        worry_level = worry_level / 3;
+
+        if is_part_1 {
+            worry_level = worry_level / 3;
+        } else {
+            worry_level = worry_level % mod_value
+        }
 
         let target_monkey_id = if worry_level % monkey.test_value == 0 {
             &monkey.true_monkey_id
@@ -159,40 +171,70 @@ fn take_turn(id: MonkeyID, monkeys: &mut HashMap<MonkeyID, Monkey>) {
     }
 
     while let Some((id, worry)) = deq.pop_front() {
-        monkeys.get_mut(&id).unwrap().items.push(worry);
+        monkeys.get_mut(&id).unwrap().items.push_back(worry);
     }
 }
 
-fn run_round<'a>(monkeys: &'a mut HashMap<MonkeyID, Monkey>) -> &'a mut HashMap<MonkeyID, Monkey> {
+fn run_round<'a>(
+    monkeys: &'a mut HashMap<MonkeyID, Monkey>,
+    is_part_1: bool,
+    mod_value: u64,
+) -> &'a mut HashMap<MonkeyID, Monkey> {
     for i in 0..monkeys.len() {
-        take_turn(i.try_into().unwrap(), monkeys);
+        take_turn(i.try_into().unwrap(), monkeys, is_part_1, mod_value);
     }
     monkeys
 }
 
-fn run_rounds(monkeys: &mut HashMap<MonkeyID, Monkey>, num_rounds: u32) {
-    for _ in 0..num_rounds {
-        run_round(monkeys);
+fn run_rounds(monkeys: &mut HashMap<MonkeyID, Monkey>, num_rounds: u32, is_part_1: bool) {
+    let lcm = monkeys
+        .iter()
+        .map(|(_, monkey)| monkey.test_value)
+        .product();
+
+    for i in 1..num_rounds + 1 {
+        run_round(monkeys, is_part_1, lcm);
+
+        if i == 1 || i == 20 || i % 1000 == 0 {
+            let inspections = get_inspections(&monkeys);
+            log::debug!("Round: {}, Inspections: {:?}", i, &inspections);
+        }
     }
 }
 
-fn main() {
-    let input = load_input();
-    let mut monkeys = parse_monkeys(&input);
-    run_rounds(&mut monkeys, 20);
-    let mut inspections: Vec<u32> = monkeys
+fn get_inspections(monkeys: &HashMap<MonkeyID, Monkey>) -> Vec<u64> {
+    let mut inspections: Vec<(MonkeyID, u64)> = monkeys
         .iter()
-        .map(|(_, monkey)| monkey.inspections)
+        .map(|(i, monkey)| (*i, monkey.inspections))
         .collect();
-    inspections.sort();
-    let monkey_business: u32 = inspections.iter().rev().take(2).product();
 
+    inspections.sort_by(|a, b| a.0.cmp(&b.0));
+
+    inspections.into_iter().map(|(_, b)| b).collect()
+}
+
+fn get_monkey_business(input: &str, num_rounds: u32, is_part_1: bool) -> u64 {
+    let mut monkeys = parse_monkeys(&input);
+    run_rounds(&mut monkeys, num_rounds, is_part_1);
+    let mut inspections: Vec<u64> = get_inspections(&monkeys);
+    inspections.sort();
+    let monkey_business: u64 = inspections.iter().rev().take(2).product();
+    monkey_business
+}
+
+fn main() {
+    env_logger::init();
+    let input = load_input();
+    let monkey_business = get_monkey_business(&input, 20, true);
     println!("{:?}", monkey_business);
+
+    let monkey_business_2 = get_monkey_business(&input, 10000, false);
+    println!("{:?}", monkey_business_2);
 }
 
 #[cfg(test)]
 mod test {
-    use std::collections::HashMap;
+    use std::collections::{HashMap, VecDeque};
 
     use crate::{parse_monkeys, Monkey, Operation, A};
 
@@ -204,7 +246,7 @@ mod test {
                 0,
                 Monkey {
                     id: 0,
-                    items: vec![79, 98],
+                    items: VecDeque::from(vec![79, 98]),
                     operation: Operation::Mul(A::X, A::Value(19)),
                     test_value: 23,
                     true_monkey_id: 2,
@@ -216,7 +258,7 @@ mod test {
                 1,
                 Monkey {
                     id: 1,
-                    items: vec![54, 65, 75, 74],
+                    items: VecDeque::from(vec![54, 65, 75, 74]),
                     operation: Operation::Add(A::X, A::Value(6)),
                     test_value: 19,
                     true_monkey_id: 2,
@@ -228,7 +270,7 @@ mod test {
                 2,
                 Monkey {
                     id: 2,
-                    items: vec![79, 60, 97],
+                    items: VecDeque::from(vec![79, 60, 97]),
                     operation: Operation::Mul(A::X, A::X),
                     test_value: 13,
                     true_monkey_id: 1,
@@ -240,7 +282,7 @@ mod test {
                 3,
                 Monkey {
                     id: 3,
-                    items: vec![74],
+                    items: VecDeque::from(vec![74]),
                     operation: Operation::Add(A::X, A::Value(3)),
                     test_value: 17,
                     true_monkey_id: 0,
