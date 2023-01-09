d3832476620f003a9dd80c5b1306254b406c68da
Harry Goatman <hg@harrygoatman.com>
2023-01-09 18:21:05 +0000

    feat(21): part 2

--- 21/src/main.rs
+++ 21/src/main.rs
@@ -32,7 +32,7 @@ fn load_input() -> String {
 }
 
 fn parse_operation(input: &str) -> Operation {
-    let mut split = input.split(" ");
+    let mut split = input.split(' ');
     let a = split.next().unwrap().to_string();
     let op = split.next().unwrap();
     let b = split.next().unwrap().to_string();
@@ -51,9 +51,9 @@ fn parse_job(input: &str) -> Job {
     let monkey_id = split.next().unwrap().to_string();
 
     let yell_str = split.next().unwrap();
-    let determine_yell: char = yell_str.chars().nth(0).unwrap();
+    let determine_yell: char = yell_str.chars().next().unwrap();
 
-    let yell = match determine_yell.is_digit(10) {
+    let yell = match determine_yell.is_ascii_digit() {
         true => Something::Number(yell_str.parse::<Value>().unwrap()),
         false => Something::MathsOperation(parse_operation(yell_str)),
     };
@@ -61,17 +61,21 @@ fn parse_job(input: &str) -> Job {
     Job { monkey_id, yell }
 }
 
-fn parse_jobs(input: &str) -> Vec<Job> {
-    input.split('\n').map(parse_job).collect()
+fn parse_jobs(input: &str) -> HashMap<MonkeyId, Something> {
+    HashMap::from_iter(
+        input
+            .split('\n')
+            .map(parse_job)
+            .map(|job| (job.monkey_id, job.yell)),
+    )
 }
 
-fn get_execution_order(job_map: &HashMap<MonkeyId, Something>) -> Vec<MonkeyId> {
+fn get_execution_order(job_map: &HashMap<MonkeyId, Something>, root: &str) -> Vec<MonkeyId> {
     let mut queue = VecDeque::new();
     let mut order = Vec::new();
 
-    let root = String::from("root");
-    queue.push_front(job_map.get(&root).unwrap());
-    order.push(root);
+    queue.push_front(job_map.get(root).unwrap());
+    order.push(root.to_string());
 
     while !queue.is_empty() {
         let job = queue.pop_back().unwrap();
@@ -97,7 +101,7 @@ fn get_execution_order(job_map: &HashMap<MonkeyId, Something>) -> Vec<MonkeyId> 
     }
 
     order.reverse();
-    log::debug!("{:?}", order);
+    log::trace!("{:?}", order);
 
     order
 }
@@ -112,13 +116,12 @@ fn unwrap_value(jobs: &HashMap<MonkeyId, Something>, monkey_id: &str) -> Value {
     panic!("Expected to unwrap a Number not an expression!");
 }
 
-fn get_root_yell(jobs: &[Job]) -> Value {
-    let mut job_map: HashMap<MonkeyId, Something> =
-        HashMap::from_iter(jobs.iter().cloned().map(|job| (job.monkey_id, job.yell)));
+fn get_yell(job_map_read: &HashMap<MonkeyId, Something>, root: &str) -> Value {
+    let mut job_map: HashMap<MonkeyId, Something> = job_map_read.clone();
 
-    log::debug!("{:#?}", job_map);
+    let execution_order = get_execution_order(&job_map, root);
 
-    let execution_order = get_execution_order(&job_map);
+    log::trace!("{:#?}", execution_order);
 
     for monkey_id in execution_order {
         let something = job_map.get(&monkey_id).unwrap();
@@ -135,27 +138,138 @@ fn get_root_yell(jobs: &[Job]) -> Value {
         }
     }
 
-    unwrap_value(&job_map, "root")
+    unwrap_value(&job_map, root)
+}
+
+fn find_path(job_map: &HashMap<MonkeyId, Something>, start: &str, goal: &str) -> Vec<MonkeyId> {
+    let mut stack = Vec::new();
+
+    stack.push((start, vec![]));
+
+    while !stack.is_empty() {
+        let (id, path) = stack.pop().unwrap();
+
+        if id == goal {
+            let mut new_path = path;
+            new_path.push(id.to_string());
+            return new_path;
+        }
+
+        let job = job_map.get(id).unwrap();
+
+        let maybe_children = match job {
+            Something::Number(_) => None,
+            Something::MathsOperation(Operation::Add(a, b))
+            | Something::MathsOperation(Operation::Subtract(a, b))
+            | Something::MathsOperation(Operation::Divide(a, b))
+            | Something::MathsOperation(Operation::Multiply(a, b)) => Some([a, b]),
+        };
+
+        if let Some(children) = maybe_children {
+            for child in children.into_iter() {
+                let mut new_path = path.clone();
+                new_path.push(id.to_string());
+                stack.push((child, new_path));
+            }
+        }
+    }
+
+    panic!("No humn found!");
+}
+
+fn get_humn_yell(jobs: &HashMap<MonkeyId, Something>) -> Value {
+    let path_to_humn = find_path(jobs, "root", "humn");
+    log::debug!("{:?}", path_to_humn);
+
+    let root_job = jobs.get("root").unwrap();
+    let maybe_children = match root_job {
+        Something::Number(_) => None,
+        Something::MathsOperation(Operation::Add(a, b))
+        | Something::MathsOperation(Operation::Subtract(a, b))
+        | Something::MathsOperation(Operation::Divide(a, b))
+        | Something::MathsOperation(Operation::Multiply(a, b)) => Some([a, b]),
+    };
+
+    let child = path_to_humn.get(1).unwrap();
+    let other = maybe_children
+        .unwrap()
+        .into_iter()
+        .find(|a| *a != child)
+        .unwrap();
+    let mut humn_yell = get_yell(jobs, other);
+    log::debug!("Yell: {}", humn_yell);
+
+    for i in 1..(path_to_humn.len() - 1) {
+        let parent = path_to_humn.get(i).unwrap();
+        let child = path_to_humn.get(i + 1).unwrap();
+
+        log::debug!("Parent: {}, Child: {}", parent, child);
+
+        if let Something::MathsOperation(parent_operation) = jobs.get(parent).unwrap() {
+            log::debug!("{:?}", parent_operation);
+
+            if let Operation::Add(a, b) = parent_operation {
+                if a == child {
+                    humn_yell -= get_yell(jobs, b);
+                } else {
+                    humn_yell -= get_yell(jobs, a);
+                }
+            }
+
+            if let Operation::Subtract(a, b) = parent_operation {
+                if a == child {
+                    humn_yell += get_yell(jobs, b);
+                } else {
+                    humn_yell = get_yell(jobs, a) - humn_yell;
+                }
+            }
+
+            if let Operation::Multiply(a, b) = parent_operation {
+                if a == child {
+                    humn_yell /= get_yell(jobs, b);
+                } else {
+                    humn_yell /= get_yell(jobs, a);
+                }
+            }
+
+            if let Operation::Divide(a, b) = parent_operation {
+                if a == child {
+                    humn_yell *= get_yell(jobs, b);
+                } else {
+                    humn_yell = get_yell(jobs, a) / humn_yell;
+                }
+            }
+        }
+        log::debug!("Yell: {}", humn_yell);
+    }
+
+    humn_yell
 }
 
 fn main() {
     env_logger::init();
     let input = load_input();
     let jobs = parse_jobs(&input);
-    let yell = get_root_yell(&jobs);
-    println!("{}", yell);
+
+    let root_yell = get_yell(&jobs, "root");
+    println!("{}", root_yell);
+
+    let humn_yell = get_humn_yell(&jobs);
+    println!("{}", humn_yell);
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{get_root_yell, parse_jobs, Job, Operation, Something};
+    use std::collections::HashMap;
+
+    use crate::{get_humn_yell, get_yell, parse_jobs, Job, MonkeyId, Operation, Something};
 
     fn init() {
         let _ = env_logger::builder().is_test(true).try_init();
     }
 
-    fn get_test_jobs() -> Vec<Job> {
-        vec![
+    fn get_test_jobs() -> HashMap<MonkeyId, Something> {
+        let job_list = vec![
             Job {
                 monkey_id: String::from("root"),
                 yell: Something::MathsOperation(Operation::Add(
@@ -237,7 +351,9 @@ mod tests {
                 monkey_id: String::from("hmdt"),
                 yell: Something::Number(32),
             },
-        ]
+        ];
+
+        HashMap::from_iter(job_list.into_iter().map(|job| (job.monkey_id, job.yell)))
     }
 
     #[test]
@@ -245,7 +361,7 @@ mod tests {
         init();
         let input = include_str!("../test.txt");
         let expected = get_test_jobs();
-        let actual = parse_jobs(&input);
+        let actual = parse_jobs(input);
         assert_eq!(expected, actual);
     }
 
@@ -254,7 +370,16 @@ mod tests {
         init();
         let input = get_test_jobs();
         let expected = 152;
-        let actual = get_root_yell(&input);
+        let actual = get_yell(&input, "root");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_get_humn_yell() {
+        init();
+        let input = get_test_jobs();
+        let expected = 301;
+        let actual = get_humn_yell(&input);
         assert_eq!(expected, actual);
     }
 }
